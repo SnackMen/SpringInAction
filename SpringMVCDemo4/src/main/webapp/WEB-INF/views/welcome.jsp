cb85ea7042112e47d3871084a0759d8e8a88e2a9
laowanggit <vipkia@sina.cn>
2016-10-19 00:12:43 +0800

    完成第一个例子

---
+++ SpringMVCDemo4/src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: laowang
+  Date: 16-10-18
+  Time: 上午11:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>演示页面</title>
+</head>
+<body>
+    Greeting : ${greeting}
+</body>
+</html>
---
+++ SpringMvcDemo5/src/main/java/com/ws/spring/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.ws.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by laowang on 16-10-18.
+ */
+@Controller
+//@RequestMapping("/")
+public class HelloWorldController {
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    public String sayHello(ModelMap modelMap){
+        modelMap.addAttribute("greeting","Hello World from Spring 4 MVC");
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/helloagain",method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap modelMap){
+        modelMap.addAttribute("greeting","Hello World Again , from Spring 4 MVC");
+        return "welcome";
+    }
+}
